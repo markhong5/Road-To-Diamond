10922f28cdd8ecfe6601a442d1cee94191b50d4e
Mark <markh5@uci.edu>
2021-01-05 16:53:46 -0800

    started to mix the grid style with player movement

--- grid.py
+++ grid.py
@@ -1,16 +1,42 @@
-""" Sprite Sample Program """
+"""
+Array Backed Grid
 
+Show how to use a two-dimensional list/array to back the display of a
+grid on-screen.
+
+Note: Regular drawing commands are slow. Particularly when drawing a lot of
+items, like the rectangles in this example.
+
+For faster drawing, create the shapes and then draw them as a batch.
+See array_backed_grid_buffered.py
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.array_backed_grid
+"""
 import arcade
-
-# --- Constants ---
 SPRITE_SCALING_BOX = 0.5
 SPRITE_SCALING_PLAYER = 0.3
-
-width, height = arcade.get_display_size()
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 900
-
 MOVEMENT_SPEED = 5
+# Set how many rows and columns we will have
+ROW_COUNT = 23
+COLUMN_COUNT = 23
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 40
+HEIGHT = 40
+
+# This sets the margin between each cell
+# and on the edges of the screen.
+MARGIN = 2
+
+# Do the math to figure out our screen dimensions
+SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
+SCREEN_TITLE = "Array Backed Grid Example"
+
+STARTING_ROW = 0
+STARTING_COL = 0
+
 class Player(arcade.Sprite):
 
     def update(self):
@@ -32,53 +58,59 @@ class Player(arcade.Sprite):
             self.top = SCREEN_HEIGHT - 1
 
 class MyGame(arcade.Window):
-    """ This class represents the main window of the game. """
+    """
+    Main application class.
+    """
 
-    def __init__(self):
-        """ Initializer """
-        # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
+    def __init__(self, width, height, title):
+        """
+        Set up the application.
+        """
 
-        # Sprite lists
+        super().__init__(width, height, title)
+
+        # Create a 2 dimensional array. A two dimensional
+        # array is simply a list of lists.
+        self.grid = []
+        for row in range(ROW_COUNT):
+            # Add an empty array that will hold each cell
+            # in this row
+            self.grid.append([])
+            for column in range(COLUMN_COUNT):
+                self.grid[row].append(0)  # Append a cell
+
+        arcade.set_background_color(arcade.color.BLACK)
         self.player_list = None
-        self.wall_list = None
-
-        # Set up the player
         self.player_sprite = None
-
-        self.physics_engine = None
-
         self.left_pressed = False
         self.right_pressed = False
         self.up_pressed = False
         self.down_pressed = False
 
-    def setup(self):
-
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
-
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
+        self.physics_engine = None
 
-        # Reset the score
-        self.score = 0
-
-        # Create the player
+    def setup(self):
+        self.player_list = arcade.SpriteList()
         self.player_sprite = Player("character.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite.width = WIDTH
+        self.player_sprite.height = HEIGHT
+        self.player_sprite.center_x = (MARGIN + WIDTH) * STARTING_COL + MARGIN + WIDTH // 2
+        self.player_sprite.center_y = (MARGIN + HEIGHT) * STARTING_ROW + MARGIN + HEIGHT // 2
         self.player_list.append(self.player_sprite)
 
         # Manually create and position a box at 300, 200
         wall = arcade.Sprite("crate.png", SPRITE_SCALING_BOX)
+        wall.width = WIDTH
+        wall.height = HEIGHT
         wall.center_x = 300
         wall.center_y = 200
         self.wall_list.append(wall)
 
         # Manually create and position a box at 364, 200
         wall = arcade.Sprite("crate.png", SPRITE_SCALING_BOX)
+        wall.width = WIDTH
+        wall.height = HEIGHT
         wall.center_x = 364
         wall.center_y = 200
         self.wall_list.append(wall)
@@ -86,9 +118,27 @@ class MyGame(arcade.Window):
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
     def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
         arcade.start_render()
-        self.wall_list.draw()
+
+        # Draw the grid
+        for row in range(ROW_COUNT):
+            for column in range(COLUMN_COUNT):
+                # Figure out what color to draw the box
+                color = arcade.color.WHITE
+
+                # Do the math to figure out where the box is
+                x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
+                y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
+
+                # Draw the box
+                arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
         self.player_list.draw()
+        self.wall_list.draw()
 
     def update(self, delta_time):
         self.player_sprite.change_x = 0
@@ -133,8 +183,7 @@ class MyGame(arcade.Window):
 
 
 def main():
-    """ Main method """
-    window = MyGame()
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
 
